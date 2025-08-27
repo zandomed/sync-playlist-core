72d0addacefe8141f829599e9eaae0ba1c188b2e
Miguel Mendoza <miguel@zandome.dev>
2025-08-27 13:43:42 +0200

    Initial Commit

---
+++ src/main/java/com/zandome/sync_playlist/SyncPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.zandome.sync_playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncPlaylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zandome/sync_playlist/SyncPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zandome.sync_playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncPlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
